b5aec6ec7682f4e72cac8e93e764490f20baf3ea
ksesly <ks.you.04@gmail.com>
2024-06-19 12:25:31 +0300

    added confirm field to jury

--- models/project.model.js
+++ models/project.model.js
@@ -1,65 +1,67 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const ScoreSchema = new Schema({
-  criteria: {
-    type: Schema.Types.ObjectId,
-    ref: 'Criteria',
-    required: true
-  },
-  score: {
-    type: Number,
-    required: true,
-    min: 0,
-    max: 10,
-    default: 0
-  }
+    criteria: {
+        type: Schema.Types.ObjectId,
+        ref: "Criteria",
+        required: true,
+    },
+    score: {
+        type: Number,
+        required: true,
+        min: 0,
+        max: 10,
+        default: 0,
+    },
 });
 
 const JureSchema = new Schema({
-  jureId: {
-    type: Schema.Types.ObjectId,
-    required: true
-  },
-  comment: {
-    type: String,
-    required: false,
-    default: ''
-  },
-  scores: [ScoreSchema]
+    jureId: {
+        type: Schema.Types.ObjectId,
+        required: true,
+    },
+    comment: {
+        type: String,
+        required: false,
+        default: "",
+    },
+    confirmed: {
+        type: Boolean,
+        required: true,
+        default: false,
+    },
+    scores: [ScoreSchema],
 });
 
 const ProjectSchema = new Schema({
-  name: {
-    type: String,
-    required: true
-  },
-  description: {
-    type: String,
-    required: true
-  },
-  project_link: {
-    type: String,
-    required: true
-  },
-  video_link: {
-    type: String,
-    required: true
-  },
-  course: {
-    type: Schema.Types.ObjectId,
-    ref: 'Course',
-    required: true
-  },
-  criterias: [
-    {
-      type: Schema.Types.ObjectId,
-      ref: 'Criteria',
-      required: true
-    }
-  ],
-  jures: [JureSchema]
+    name: {
+        type: String,
+        required: true,
+    },
+
+    project_link: {
+        type: String,
+        required: true,
+    },
+    video_link: {
+        type: String,
+        required: true,
+    },
+    course: {
+        type: Schema.Types.ObjectId,
+        ref: "Course",
+        required: true,
+    },
+    criterias: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: "Criteria",
+            required: true,
+        },
+    ],
+    jures: [JureSchema],
 });
 
-const Project = mongoose.model('Project', ProjectSchema);
+const Project = mongoose.model("Project", ProjectSchema);
 module.exports = Project;
